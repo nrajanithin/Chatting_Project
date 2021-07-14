32ea278483f3e0cac72cd1a0de68e3b967ccbe42
nrajanithin <nrajanithin@gmail.com>
2021-07-14 09:18:47 -0400

    Modified Firebase APIs

--- src/components/fire.js
+++ src/components/fire.js
@@ -1,12 +1,6 @@
 import firebase from 'firebase'
 var firebaseConfig = {
-  apiKey: "AIzaSyB_WHEMl33WLvxWpIgsThf5IEoJpkd7F3E",
-  authDomain: "chatting-41976.firebaseapp.com",
-  projectId: "chatting-41976",
-  storageBucket: "chatting-41976.appspot.com",
-  messagingSenderId: "953847033845",
-  appId: "1:953847033845:web:31164fed11b6b9663cbabc",
-  databaseURL:"https://chatting-41976-default-rtdb.firebaseio.com/"
+  //Paste your firebase APIs
 };
 const fire = firebase.initializeApp(firebaseConfig);
 export default fire;
